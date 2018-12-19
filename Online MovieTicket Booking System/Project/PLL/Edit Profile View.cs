89b91ce4867c19bdf3dad3af3e5621f123d38bb9
Shornob <34908432+Shornob@users.noreply.github.com>
2018-12-19 17:34:46 +0600

    Online MovieTicket Booking System
    C# 3tier Architecture and MySQL sever were used

---
+++ Online MovieTicket Booking System/Project/PLL/Edit Profile View.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Project.BLL;
+using Project.PLL;
+
+namespace MovieTicketBookingSystem.PLL
+{
+    public partial class Edit_Profile : MetroFramework.Forms.MetroForm
+    {
+        employee emp = new employee();
+        Form previousForm;
+
+        public Edit_Profile(Form previousForm, employee emp)
+        {
+            InitializeComponent();
+            this.emp = emp;
+            this.previousForm = previousForm;
+        }
+
+
+        private void Logout_button_Click(object sender, EventArgs e)
+        {
+            FormLogin fl = new FormLogin();
+            this.Hide();
+            fl.Show();
+        }
+
+        private void Back_button_Click(object sender, EventArgs e)
+        {
+            previousForm.Show();
+            this.Hide();
+        }
+
+        private void Edit_Profile_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Edit_Profile_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool VerifyEmail(string email)
+        {
+            return Regex.IsMatch(email, @".+@.+\...+", RegexOptions.IgnoreCase);
+        }
+
+        private void Update_button_Click(object sender, EventArgs e)
+        {
+            string email = textBoxEmail.Text;
+            string phone = textBoxPhone.Text;
+            string address = richTextBoxAddress.Text;
+
+            if (!VerifyEmail(email))
+            {
+                MessageBox.Show("Invalid email address");
+                return;
+            }
+
+            try
+            {
+                // string id, string name, string email, string address, int sts, string phone, float salary
+                if (emp.UpdatePerson(emp.employeeId, emp.Name, email, address, int.Parse(emp.role), phone, emp.salary))
+                {
+                    MessageBox.Show("Profile updated");
+
+                    var f = (IEmployee)previousForm;
+                    f.Emp.mobileNo = phone;
+                    f.Emp.address = address;
+                    f.Emp.email = email;
+                    f.Reload();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to update profile");
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Failed to update profile. Invalid input(s).");
+                MessageBox.Show(exc.Message);
+            }
+            textBoxEmail.Text = textBoxPhone.Text = richTextBoxAddress.Text = string.Empty;
+        }
+    }
+}
+
---
+++ Online MovieTicket Booking System/Project/PLL/Welcome.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MovieTicketBookingSystem.PLL
+{
+    partial class Welcome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Exit_button = new System.Windows.Forms.Button();
+            this.Login_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Exit_button
+            // 
+            this.Exit_button.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Exit_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Exit_button.FlatAppearance.BorderSize = 0;
+            this.Exit_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Exit_button.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit_button.Location = new System.Drawing.Point(694, 494);
+            this.Exit_button.Name = "Exit_button";
+            this.Exit_button.Size = new System.Drawing.Size(73, 33);
+            this.Exit_button.TabIndex = 92;
+            this.Exit_button.Text = "Exit";
+            this.Exit_button.UseVisualStyleBackColor = false;
+            this.Exit_button.Click += new System.EventHandler(this.Exit_button_Click);
+            // 
+            // Login_button
+            // 
+            this.Login_button.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Login_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Login_button.FlatAppearance.BorderSize = 0;
+            this.Login_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Login_button.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login_button.Location = new System.Drawing.Point(558, 494);
+            this.Login_button.Name = "Login_button";
+            this.Login_button.Size = new System.Drawing.Size(73, 33);
+            this.Login_button.TabIndex = 91;
+            this.Login_button.Text = "Login";
+            this.Login_button.UseVisualStyleBackColor = false;
+            this.Login_button.Click += new System.EventHandler(this.Login_button_Click);
+            // 
+            // Welcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Project.Properties.Resources._1pexels_photo_375885;
+            this.BackImage = global::Project.Properties.Resources._1pexels_photo_375885;
+            this.ClientSize = new System.Drawing.Size(807, 571);
+            this.Controls.Add(this.Exit_button);
+            this.Controls.Add(this.Login_button);
+            this.Name = "Welcome";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Welcome_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Exit_button;
+        private System.Windows.Forms.Button Login_button;
+    }
+}
